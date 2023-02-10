10642b774bf665577771e23ccd90af6738feb85a
Rishi Varma Kalidindi <rishivarma.lc.dev@gmail.com>
2023-02-10 18:09:22 -0500

    Project Created

---
+++ LAB_rishivarma_c0880853_android/app/src/main/java/ca/lcit22f/rishivarma853/lab_rishivarma_c0880853_android/MainActivity.java
@@ -0,0 +1,14 @@
+package ca.lcit22f.rishivarma853.lab_rishivarma_c0880853_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
